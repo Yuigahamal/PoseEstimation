5768fe38be57e25c7d4d723c3c437eef0b138fe1
YuigahamaYui <1370836538@qq.com>
2026-07-11 20:36:58 +0800

    second commit

--- CIFAR-10/src/config.py
+++ CIFAR-10/src/config.py
@@ -12,7 +12,7 @@ DATA_FRACTION = 1.0  # 训练数据量，1.0 表示全量
 BATCH_SIZE = 16 # 批次大小
 NUM_WORKERS = 8 # 工作线程数
 EPOCHS = 10 # 训练轮数
-LR = 0.001 # 学习率
+LR = 0.005 # 学习率
 
 
 
--- CIFAR-10/src/evaluate.py
+++ CIFAR-10/src/evaluate.py
@@ -1,7 +1,28 @@
 import torch
 from tqdm import tqdm
+from model import resnet50
+import dataset
+import config
+import os
+def run_evaluate():
+    # 1.获取验证集
+    val_dataloder =  dataset.get_dataloder(is_train=False)
 
 
+    # 2.获取设备
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device_name = torch.cuda.get_device_name(0) if device.type == "cuda" else "CPU"
+    print(f"训练设备为: {device_name}")
+
+    # 3.加载模型
+    model = resnet50().to(device)
+    if os.path.exists(config.WEIGHT_DIR / "best_acc.pth"):
+        model.load_state_dict(torch.load(config.WEIGHT_DIR / "best_acc.pth"))
+
+    # 4.评估模型
+    acc = evaluate(model, val_dataloder, device)
+    print(f"模型在验证集上的准确率为: {acc:.4f}")
 
 # 图像分类评估函数,返回准确率
 def evaluate(model, val_loader, device):
@@ -22,3 +43,6 @@ def evaluate(model, val_loader, device):
             pbar.set_postfix(acc=correct / total)
 
     return correct / total
+
+if __name__ == '__main__':
+    run_evaluate()
